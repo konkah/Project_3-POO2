ade0555e0250e1b60c13a4140a1a97de32573c21
Karlos Helton Braga <karlos.hbraga@gmail.com>
2020-12-20 08:27:06 -0300

    Add function to change traffic light to client GUI

--- source/src/client/TrafficLightClientWindow.java
+++ source/src/client/TrafficLightClientWindow.java
@@ -1,7 +1,11 @@
 package client;
 
+import common.TrafficLightState;
+
 import javax.swing.*;
 import java.awt.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class TrafficLightClientWindow {
     private JPanel panel;
@@ -9,9 +13,37 @@ public class TrafficLightClientWindow {
     private JLabel red;
     private JLabel yellow;
     private JLabel green;
+    private TrafficLightState currentState = TrafficLightState.RED;
 
     public static Container Create() {
         TrafficLightClientWindow instance = new TrafficLightClientWindow();
+        instance.turnOn();
         return instance.panel;
+    }
+
+    public void next() {
+        currentState = currentState.getNext();
+        turnOn();
+    }
+
+    private void turnOn() {
+        setIcon(red, TrafficLightState.RED);
+        setIcon(yellow, TrafficLightState.YELLOW);
+        setIcon(green, TrafficLightState.GREEN);
+    }
+
+    private void setIcon(JLabel label, TrafficLightState state) {
+        System.out.println(label);
+        System.out.println(state);
+        System.out.println(this.currentState);
+
+        String color = state.toString().toLowerCase();
+        String onOff = state == this.currentState ? "on" : "off";
+        String file = color + "_" + onOff + ".png";
+
+        Path path = Paths.get("src", "resources", file);
+        String absolutePath = path.toAbsolutePath().toString();
+
+        label.setIcon(new ImageIcon(absolutePath));
     }
 }

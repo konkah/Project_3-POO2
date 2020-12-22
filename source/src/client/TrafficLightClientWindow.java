1127f5ea970ce5399512c8db4a1129c68c5d2dec
Karlos Helton Braga <karlos.hbraga@gmail.com>
2020-12-22 01:38:08 -0300

    Make yellow blink when client is not working

--- source/src/client/TrafficLightClientWindow.java
+++ source/src/client/TrafficLightClientWindow.java
@@ -1,9 +1,14 @@
 package client;
 
+import common.Interval;
 import common.TrafficLightState;
 
 import javax.swing.*;
 import java.net.URL;
+
+import static common.Settings.WAIT_MILLISECONDS;
+import static common.TrafficLightState.NOT_WORKING;
+import static common.TrafficLightState.YELLOW;
 
 /**
  * Main GUI of the server program
@@ -17,10 +22,27 @@ public class TrafficLightClientWindow {
 
     private JLabel legend;
 
-    private TrafficLightState currentState = TrafficLightState.NOT_WORKING;
+    private TrafficLightState currentState = NOT_WORKING;
+    boolean blinkOn = false;
 
     public TrafficLightClientWindow(String code) {
         legend.setText("code: " + code);
+        blinkIfNotWorking();
+    }
+
+    private void blinkIfNotWorking() {
+        Thread thread = new Thread(new Interval(WAIT_MILLISECONDS/2) {
+            @Override
+            protected void execute() {
+                if (currentState != NOT_WORKING)
+                    return;
+
+                setIcon(yellow, YELLOW, blinkOn);
+                blinkOn = !blinkOn;
+            }
+        });
+
+        thread.start();
     }
 
     /**
@@ -46,8 +68,12 @@ public class TrafficLightClientWindow {
     }
 
     private void setIcon(JLabel label, TrafficLightState state) {
+        setIcon(label, state, state == currentState);
+    }
+
+    private void setIcon(JLabel label, TrafficLightState state, boolean on) {
         String color = state.toString().toLowerCase();
-        String onOff = state == currentState ? "on" : "off";
+        String onOff = on ? "on" : "off";
         String file = "/" + color + "_" + onOff + ".png";
 
         URL url = getClass().getResource(file);
